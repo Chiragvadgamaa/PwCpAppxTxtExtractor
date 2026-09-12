dcf7575119ebd72571e2bb57e43cfee6cb591acd
Chiragvadgamaa <115563333+Chiragvadgamaa@users.noreply.github.com>
2026-09-12 11:27:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "29940427"
+api_hash = "b0390f6a1bceaa5ed4c62bb6f7d62820"
+bot_token = "8937469405:AAG5t8cOmdauW5b1QipNZvxtqQl_CQ7L1uU"
+auth_users = [8355641946]
